57f389a4b50e1aeec5e1f86af177797b482e16ea
Harlekin06 <frank@eilts.biz>
2023-11-29 10:55:47 +0100

    Small Fix

--- src/main/java/com/intuit/graphql/filter/visitors/JpaSpecificationExpressionVisitor.java
+++ src/main/java/com/intuit/graphql/filter/visitors/JpaSpecificationExpressionVisitor.java
@@ -51,7 +51,6 @@ public class JpaSpecificationExpressionVisitor<T> implements ExpressionVisitor<S
 
 	public JpaSpecificationExpressionVisitor(final Map<String, String> fieldMap,
 			final FieldValueTransformer fieldValueTransformer) {
-		System.out.println("JpaSpecificationExpressionVisitor");
 		this.fieldMap = fieldMap;
 		this.fieldStack = new ArrayDeque<>();
 		this.fieldValueTransformer = fieldValueTransformer;
